5ed1a187f9c87b329fe0fe6fb0cc2cf31b50324b
aa0241 <77753344+aa0241@users.noreply.github.com>
2022-06-07 20:14:53 +0300

    Add category controllers and routes

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const connectDb = require("./database");
 const express = require("express");
 const app = express();
 const userRoutes = require("./api/users/users.routes");
+const categoriesRoutes = require("./api/categories/categories.routes");
 const passport = require("passport");
 const cors = require("cors");
 const { localStrategy, jwtStrategy } = require("./middleware/passport");
@@ -13,6 +14,7 @@ app.use(passport.initialize());
 passport.use(localStrategy);
 passport.use(jwtStrategy);
 app.use(userRoutes);
+app.use(categoriesRoutes);
 
 app.use((req, res, next) => {
   const err = new Error("Not Found");
